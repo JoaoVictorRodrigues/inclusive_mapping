e4844293c25f8c4f4c85a789cbab673df16e800e
André Oliveira Gonçalves <1191660@isep.ipp.pt>
2024-04-11 10:51:22 +0100

    Mobile Home page constructed but not functional

--- MyTestApp/View/Home.tsx
+++ MyTestApp/View/Home.tsx
@@ -1,15 +1,96 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import { Button, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 const HomePage = () => {
     return (
-        <View style={{flex: 1, backgroundColor: 'black'}}>
-            <View style={{
-                flex: 1, 
-                //justifyContent: 'flex-start',
-            /*alignItems: 'flex-start'*/}}>
-                    <Text style={{alignSelf: 'flex-start'}}>Back</Text>
-                    <Text style={{alignSelf: 'flex-end'}}>Logout</Text> </View>
+        <View style={{ flex: 1, backgroundColor: 'black'}}>
+            <View id='Header' style={styles.pageHeader}>
+                <Text style={styles.headerLabelsLeft}>Back</Text>
+                <Text style={styles.headerLabelsRight}>Logout</Text> 
+            </View>
+            <View id='ImageHolder' style={styles.imageHolder}>
+                <Image style={styles.schoolLogo} 
+                    source={require('./images/pngegg.png')}></Image>
+            </View>
+            <View id='BtnContainer' 
+                  style={styles.btnContainer}>
+                    <View style={styles.whiteRect}>
+                        <TouchableOpacity style={styles.btnOrange}>
+                            <Text style={styles.btnLabelOrange}>Map</Text>    
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.btnGray}>
+                            <Text style={styles.btnLabelGray}>Virtual Diary</Text>    
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.btnOrange}>
+                            <Text style={styles.btnLabelOrange}>Profile</Text>    
+                        </TouchableOpacity>      
+                    </View> 
+            </View>
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    pageHeader:{
+        flex: 1, 
+        flexDirection: 'row', 
+        justifyContent:'space-between', 
+        paddingTop: 58, 
+        height: 36
+    },
+    headerLabelsLeft: {
+        direction:'ltr', 
+        color: '#FFFFFF' 
+    },
+    headerLabelsRight: {
+        direction:'rtl', 
+        color: '#FFFFFF' 
+    },
+    imageHolder: {
+        flex: 2,  
+        justifyContent: 'center', 
+        alignContent: 'center'
+    },
+    schoolLogo:{
+        height: 318, 
+        width: 318, 
+        alignSelf: 'center'
+    },
+    btnContainer:{
+        flex: 3,  
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    whiteRect: {
+        width: 333, 
+        height: 250, 
+        backgroundColor: '#FFFFFF',
+        alignItems: 'center',
+        justifyContent: 'space-evenly',
+        borderRadius: 10
+    },
+    btnOrange:{
+        backgroundColor: '#B43920',
+        borderRadius: 4,
+        width: 301.93,
+        height: 49.31,  
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    btnLabelOrange:{
+        color: '#FFFFFF'
+    },
+    btnGray:{
+        backgroundColor: '#E6E6E6',
+        borderRadius: 4,
+        width: 301.93,
+        height: 49.31,  
+        alignItems: 'center',
+        justifyContent: 'center'        
+    },
+    btnLabelGray:{
+        color: '#000000'
+    }
+})
+
+export default HomePage
