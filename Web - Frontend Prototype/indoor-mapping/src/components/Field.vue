97b61d692b5b19f97866f33602f45be19cb10736
EduardoJoseSecaMonteiro <eduardosecamonteiro@gmail.com>
2024-04-17 22:42:18 +0100

    Development of Sign Up Page (Mostly visual - still a Mock up)

---
+++ Web - Frontend Prototype/indoor-mapping/src/components/Field.vue
@@ -0,0 +1,34 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <div class="flex flex-col items-start">
+    <label class="text-black mt-6" :for="f_id">{{ f_label }}</label>
+    <div class="flex w-full">
+      <input
+        class="w-full rounded-md p-1 border"
+        :class="f_icon ? 'pl-10' : 'pl-2'"
+        :id="f_id"
+        :type="f_type"
+        :name="f_id"
+        :placeholder="f_placeholder"
+      />
+      <i v-if="f_icon" :class="'fa ' + f_icon + ' fieldIcon'"></i>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    f_id: { type: String, required: true },
+    f_label: { type: String, required: true },
+    f_type: { type: String, required: true },
+    f_placeholder: { type: String, default: '' },
+    f_icon: { type: String, default: '' }
+  },
+  methods: {
+    handleSideBar() {
+      this.showSideBar = !this.showSideBar
+    }
+  }
+}
+</script>
--- Web - Frontend Prototype/indoor-mapping/src/views/SignPage.vue
+++ Web - Frontend Prototype/indoor-mapping/src/views/SignPage.vue
@@ -1,44 +1,76 @@
 <template>
-  <div class="container relative my-8 mx-auto border-8 border-black dark:border-red-600">
-    <div
-      class="h-screen bg-cover bg-no-repeat bg-bottom bg-[url('../../public/images/ISEP_PortoAP9I0151-v3.jpg')]"
-    >
-      <div class="sticky top-0 float-right bg-white/50 h-full w-2/6 py-20 px-16">
-        <form action="" class="flex flex-col">
-          <label for="name" class="flex-none text-black mt-6">Name</label>
-          <input
-            type="text"
-            id="signInput"
-            name="name"
-            class="flex-none rounded p-1"
-            placeholder="Enter your name"
-          />
-
-          <label for="email" class="flex-start text-black mt-6">Email</label>
-          <input
-            type="text"
-            id="signInput"
-            name="email"
-            class="flex-none rounded p-1"
-            placeholder="Enter your email"
-          />
-        </form>
-        <button class="mt-8 p-2 bg-red-700 text-white font-bold" @click="signIn()">Sign In (Test)</button>
+  <div
+    class="h-screen bg-cover bg-no-repeat bg-bottom bg-[url('../../public/images/ISEP_PortoAP9I0151-v3.jpg')]"
+  >
+    <!-- White Section -->
+    <div class="flex flex-col justify-evenly top-0 float-right bg-white/90 h-full w-2/5 py-10 px-16 rounded">
+      <div class="w-full h-20 flex flex-row justify-between items-center">
+        <div class="h-full flex flex-col justify-between items-start">
+          <h1>Sign up</h1>
+          <p>Never get lost when in ISEP</p>
+        </div>
+        <img class="w-36 object-cover h-full" src="../../public/images/ISEP_redLogo.jpg" alt="" />
       </div>
-      
+      <form action="" class="flex flex-col">
+        <!-- Name -->
+        <Field
+          f_id="signIn_Name"
+          f_label="Name"
+          f_type="text"
+          f_placeholder="Enter your name"
+          f_icon="fa-user-circle"
+        ></Field>
+        <!-- Email -->
+        <Field
+          f_id="signIn_Email"
+          f_label="Email"
+          f_type="text"
+          f_placeholder="Enter your email"
+          f_icon="fa-envelope-o"
+        ></Field>
+        <!-- Password -->
+        <Field
+          f_id="signIn_Password"
+          f_label="Password"
+          f_type="password"
+          f_placeholder="Enter your password"
+          f_icon="fa-lock"
+        ></Field>
+        <!-- Confirm Password -->
+        <Field
+          f_id="signIn_ConfirmPassword"
+          f_label="Confirm Password"
+          f_type="password"
+          f_placeholder="Confirm your password"
+          f_icon="fa-lock"
+        ></Field>
+      </form>
+      <div>
+        <button
+          class="w-full rounded-md mt-8 p-2 bg-red-700 text-white font-bold"
+          @click="signIn()"
+        >
+          Sign In (Test)
+        </button>
+        <p class="m-4">Already have an account? Sign in</p>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import Field from '../components/Field.vue'
+
 export default {
-  components: {},
+  components: {
+    Field
+  },
   data() {
     return {}
   },
   methods: {
-    signIn() {      
-      this.$emit("signIn");
+    signIn() {
+      this.$emit('signIn')
     }
   }
 }
