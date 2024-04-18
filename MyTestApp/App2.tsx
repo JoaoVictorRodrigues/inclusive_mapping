4bfd6d4aa7b441bda8343f4070bec39e1c61f16d
PaulofmRodrigues <paulofilipe_100pt@hotmail.com>
2024-04-18 21:55:06 +0100

    ...

--- MyTestApp/App2.tsx
+++ MyTestApp/App2.tsx
@@ -1,18 +1,22 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LandingPage from './View/LandingPage';
+import Register from './View/Register';
 import HomePage from "./View/Home";
 import ProfilePage from './View/ProfileScreen';
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
-return <NavigationContainer>
-  <Stack.Navigator initialRouteName='Home'>
-    <Stack.Screen options={{headerShown: false}} name='Home' component={HomePage}></Stack.Screen>
-    <Stack.Screen options={{headerShown: false}} name='Profile' component={ProfilePage}></Stack.Screen>
-  </Stack.Navigator>
-</NavigationContainer>
+  return <NavigationContainer>
+    <Stack.Navigator initialRouteName='Home'>
+      <Stack.Screen options={{ headerShown: false }} name='Landing Page' component={LandingPage}></Stack.Screen>
+      <Stack.Screen options={{ headerShown: false }} name='Register' component={Register}></Stack.Screen>
+      <Stack.Screen options={{ headerShown: false }} name='Home' component={HomePage}></Stack.Screen>
+      <Stack.Screen options={{ headerShown: false }} name='Profile' component={ProfilePage}></Stack.Screen>
+    </Stack.Navigator>
+  </NavigationContainer>
 };
 
 export default App
---
+++ MyTestApp/View/LandingPage.tsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+const LandingPage = ({ navigation }: { navigation: any }) => {
+    return (
+        <View style={styles.container}>
+            <View id='ImageHolder' style={styles.imageHolder}>
+                <Image style={styles.schoolLogo}
+                    source={require('./images/pngegg.png')}></Image>
+            </View>
+            <View id='BtnContainer' style={styles.btnContainer}>
+                <TouchableOpacity style={styles.btnRegister} onPress={() => navigation.navigate("Register")}>
+                    <Text style={styles.btnLabelRegister}>Register</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.btnLogin} onPress={() => navigation.navigate("Login")}>
+                    <Text style={styles.btnLabelLogin}>Login</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#9a1924',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    imageHolder: {
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    schoolLogo: {
+        width: 200,
+        height: 200,
+    },
+    btnContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: "25%"
+    },
+    btnRegister: {
+        backgroundColor: '#E89865',
+        width: "40%",
+        height: 50,
+        padding: 10,
+        margin: 5,
+        borderRadius: 5,
+        justifyContent: 'center', // centraliza o texto verticalmente
+    },
+    btnLabelRegister: {
+        color: 'white',
+        fontSize: 16,
+        fontWeight: 'bold',
+        textAlign: 'center', // centraliza o texto horizontalmente
+    },
+    btnLogin: {
+        backgroundColor: 'white',
+        width: "40%",
+        height: 50,
+        padding: 10,
+        margin: 5,
+        borderRadius: 5,
+        justifyContent: 'center', // centraliza o texto verticalmente
+    },
+    btnLabelLogin: {
+        color: 'black',
+        fontSize: 16,
+        fontWeight: 'bold',
+        textAlign: 'center', // centraliza o texto horizontalmente
+    }
+})
+
+export default LandingPage;
