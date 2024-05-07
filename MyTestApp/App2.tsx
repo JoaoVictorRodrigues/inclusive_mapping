9a951bf35ce57687a9fce352e13615b47dc72fbb
Paulo  Rodrigues <paulofilipe_100pt@hotmail.com>
2024-05-07 19:05:18 +0100

    Novo commit

--- MyTestApp/App2.tsx
+++ MyTestApp/App2.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LandingPage from './View/LandingPage';
-import Register from './View/Register';
-import HomePage from "./View/Home";
-import ProfilePage from './View/ProfileScreen';
+import LandingPage from './src/View/LandingPage';
+import Register from './src/View/Register';
+import Login from './src/View/Login';
+import HomePage from "./src/View/Home";
+import ProfilePage from './src/View/ProfileScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -13,6 +14,7 @@ const App = () => {
     <Stack.Navigator initialRouteName='Home'>
       <Stack.Screen options={{ headerShown: false }} name='Landing Page' component={LandingPage}></Stack.Screen>
       <Stack.Screen options={{ headerShown: false }} name='Register' component={Register}></Stack.Screen>
+      <Stack.Screen options={{ headerShown: false }} name='Login' component={Login}></Stack.Screen>
       <Stack.Screen options={{ headerShown: false }} name='Home' component={HomePage}></Stack.Screen>
       <Stack.Screen options={{ headerShown: false }} name='Profile' component={ProfilePage}></Stack.Screen>
     </Stack.Navigator>
