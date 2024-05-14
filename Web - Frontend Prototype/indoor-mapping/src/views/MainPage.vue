19baeabfa21e4da7e524bfaf0398047631473a40
NunoOliv2000 <152287605+NunoOliv2000@users.noreply.github.com>
2024-05-14 16:52:28 +0100

    Navbar correction

--- Web - Frontend Prototype/indoor-mapping/src/views/MainPage.vue
+++ Web - Frontend Prototype/indoor-mapping/src/views/MainPage.vue
@@ -2,12 +2,12 @@
 <template>
   <div id="nav" class="w-full h-16 dark:bg-navbarBase flex flex-row">
     <!--NavBar Title-->
-    <div class="basis-1/4 flex flex-row">
+    <div class="basis-1/6 hidden sm:flex sm:flex-row">
       <img class="hidden sm:block w-32 object-cover h-full" src="/images/ISEP_redLogo.jpg" alt="" />
       <p class="hidden lg:block self-center text-white text-base font-bold mr-2">Indoor Mapping</p>
     </div>
     <!--NavBar Buttons-->
-    <div class="basis-3/5 flex flex-row w-3/4 sm:w-3/5 justify-items-start">
+    <div class="basis-3/5 flex flex-row w-3/4 sm:w-3/5 items-center justify-start">
       <router-link
         v-for="route in routes"
         :key="route.link.path"
@@ -16,7 +16,7 @@
         :class="route.open ? 'w-60 bg-navBarHover' : 'w-20'"
         @click="closeButtonNav(route)"
       >
-        <div class="relative flex flex-row items-center h-full w-full">
+        <div class="relative flex flex-row items-center justify-start h-full w-full">
           <i :class="'fa ' + route.icon + ' fieldIcon '"></i>
           <p
             v-if="route.open"
@@ -29,17 +29,21 @@
     </div>
     <!--NavBar Profile-->
     <div
-      class="basis-1/5 flex flex-row-reverse items-center justify-start "
+      class="basis-1/5 items-center justify-start"
       :class="profile.open ? 'bg-navBarHover' : 'bg-navbarBase'"
     >
-      <router-link :to="profile.link.path" @click="closeRoutes()">
+      <router-link
+        class="relative flex flex-row-reverse items-center justify-start h-full w-full"
+        :to="profile.link.path"
+        @click="closeRoutes()"
+      >
         <!--Image-->
         <img
           class="w-12 h-12 p-1 rounded-full justify-self-center mr-2"
           src="/images/default_avatar.png"
           alt=""
         />
-        <p class="justify-self-center text-white text-base font-bold">Name</p>
+        <p class="justify-self-center text-white text-base font-bold mr-4">Name</p>
       </router-link>
     </div>
   </div>
